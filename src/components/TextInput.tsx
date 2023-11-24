9469905a1b50e164a616983b8afab82f9dd3a089
YanAnderson-bit <yan.anderson135@gmail.com>
2023-11-24 15:51:35 -0300

    Apply code review changes

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -3,5 +3,5 @@ import { Input, InputProps } from '@chakra-ui/react';
 type TextInputProps = InputProps;
 
 export const TextInput = ({ ...props }: TextInputProps) => (
-  <Input {...props} type="text" variant="text" />
+  <Input variant="text" {...props} type="text" />
 );
