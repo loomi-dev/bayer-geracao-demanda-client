033edfaa86abe47e3eaa72e2669606634d758841
YanAnderson-bit <yan.anderson135@gmail.com>
2023-11-27 11:43:31 -0300

    Uses pages layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next';
-
 import { LayoutWithoutNotifications } from '@/layouts';
 import { Landing } from '@/modules/misc/pages/Landing';
 
-const Home: NextPage = () => (
-  <LayoutWithoutNotifications>
-    <Landing />
-  </LayoutWithoutNotifications>
-);
-export default Home;
+import { NextPageWithLayout } from './_app';
+
+const Page: NextPageWithLayout = () => <Landing />;
+
+Page.getLayout = function getLayout(page) {
+  return <LayoutWithoutNotifications>{page}</LayoutWithoutNotifications>;
+};
+export default Page;
