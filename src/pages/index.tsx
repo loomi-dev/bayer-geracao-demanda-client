1af7421d0b3b4027cf0de102fb7533e204ce7774
Bruno Guerra <bruno.dev8801@gmail.com>
2023-11-27 21:59:57 -0300

    feat: radio component added in theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { Box, Radio } from '@chakra-ui/react';
+
 import { LayoutWithoutNotifications } from '@/layouts';
 
 import { NextPageWithLayout } from './_app';
 
-const Page: NextPageWithLayout = () => <></>;
+const Page: NextPageWithLayout = () => (
+  <Box ml="1rem" mt="1rem">
+    <Radio>
+      Declaro que li e concordo com os termos da Política de privacidade e entendo como coletam e
+      utilizam os dados pessoais dos usuários do App
+    </Radio>
+  </Box>
+);
 
 Page.getLayout = function getLayout(page) {
   return <LayoutWithoutNotifications>{page}</LayoutWithoutNotifications>;
