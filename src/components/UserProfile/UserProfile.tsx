a96d620765122e100ad8577cc932a8c51f18e3de
YanAnderson-bit <yan.anderson135@gmail.com>
2023-11-28 09:07:35 -0300

    Apply code review changes

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -23,7 +23,12 @@ export const UserProfile = () => {
         </Text>
         <Menu placement="bottom" isOpen={isOpen} onClose={onClose}>
           <MenuButton onClick={onOpen}>
-            <ChevronDownIcon width="2rem" height="2rem" />
+            <HStack gap="1rem">
+              <Text textStyle="footnote" color="text.footnote">
+                Opções
+              </Text>
+              <ChevronDownIcon width="2rem" height="2rem" />
+            </HStack>
           </MenuButton>
           <MenuList bg="surface.primary" shadow="none">
             <MenuItem _hover={{ opacity: '0.7' }} bg="surface.primary" color="text.primary">
