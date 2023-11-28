7d12621dee2e7404160e73303c60f95eab1a9bf8
Bruno Guerra <bruno.dev8801@gmail.com>
2023-11-27 22:09:54 -0300

    fix: removed example radio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
-import { Box, Radio } from '@chakra-ui/react';
-
 import { LayoutWithoutNotifications } from '@/layouts';
 
 import { NextPageWithLayout } from './_app';
 
-const Page: NextPageWithLayout = () => (
-  <Box ml="1rem" mt="1rem">
-    <Radio>
-      Declaro que li e concordo com os termos da Política de privacidade e entendo como coletam e
-      utilizam os dados pessoais dos usuários do App
-    </Radio>
-  </Box>
-);
+const Page: NextPageWithLayout = () => <></>;
 
 Page.getLayout = function getLayout(page) {
   return <LayoutWithoutNotifications>{page}</LayoutWithoutNotifications>;
