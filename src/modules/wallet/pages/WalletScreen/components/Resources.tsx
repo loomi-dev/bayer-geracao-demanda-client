9bd64705b634c3689086fe10d2993062189afa05
YanAnderson-bit <yan.anderson135@gmail.com>
2023-11-28 15:25:46 -0300

    Creates First action

--- src/modules/wallet/pages/WalletScreen/components/Resources.tsx
+++ src/modules/wallet/pages/WalletScreen/components/Resources.tsx
@@ -1,7 +1,48 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+
+import { AddInsideCircleIcon, CircleIcon } from '@/components';
 
 export const Resources = () => (
-  <Flex flexDir="row">
+  <Flex flexDir="column" gap="1.7rem">
     <Text textStyle="h2">Transforme seus recursos em ações</Text>
+    <Flex gap="1.6rem">
+      <Flex
+        position="relative"
+        w="22.5rem"
+        h="25.4rem"
+        borderRadius="3.2rem"
+        boxShadow="third"
+        border="1px solid"
+        borderColor="opacity.black.0.10"
+        overflow="hidden"
+      >
+        <Image
+          src="/assets/images/action1.webp"
+          quality={100}
+          fill
+          alt="ação 1"
+          style={{ objectFit: 'cover' }}
+        />
+        <Button
+          pos="absolute"
+          bottom="1.4rem"
+          right="1rem"
+          w="15.2rem"
+          h="5.1rem"
+          pr="0rem"
+          variant="third"
+          rightIcon={
+            <CircleIcon>
+              <AddInsideCircleIcon />
+            </CircleIcon>
+          }
+        >
+          <Text as="span" w="full">
+            Adicionar
+          </Text>
+        </Button>
+      </Flex>
+    </Flex>
   </Flex>
 );
--- src/styles/theme/foundations/shadows.ts
+++ src/styles/theme/foundations/shadows.ts
@@ -3,5 +3,6 @@ import { ChakraTheme } from '@chakra-ui/react';
 export const shadows: ChakraTheme['shadows'] = {
   primary: '0px 0px 24px 0px rgba(0, 0, 0, 0.10)',
   secondary: '0px 0px 30px 0px rgba(0, 0, 0, 0.15)',
+  third: '30px 30px 40px 0px rgba(0, 0, 0, 0.05)',
   outline: '0 0 0 3px black',
 };
