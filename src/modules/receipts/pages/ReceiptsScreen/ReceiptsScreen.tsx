21855e761f6cd2ceabd367be74df4c36b5ebe92e
Erick Paiva <erickpaivaasilva@gmail.com>
2023-11-30 16:30:36 -0400

    feat: set up the receipt screen

--- src/modules/receipts/pages/ReceiptsScreen/ReceiptsScreen.tsx
+++ src/modules/receipts/pages/ReceiptsScreen/ReceiptsScreen.tsx
@@ -1,3 +1,22 @@
+import { Box, Flex } from '@chakra-ui/react';
 import React from 'react';
 
-export const ReceiptsScreen = () => <></>;
+import { DocumentIcon, Header, Sidebar } from '@/components';
+
+import { TableFilter } from './components';
+import { DrawerExpenseReceipt } from './components/DrawerExpenseReceipt';
+
+export const ReceiptsScreen = () => (
+  <Flex boxSize="100%">
+    <DrawerExpenseReceipt />
+    <Sidebar
+      containerProps={{
+        position: 'initial',
+      }}
+    />
+    <Box pl="6.4rem" pr="9.343rem" pt="3.8rem" pb="3.3rem" flex={1}>
+      <Header icon={<DocumentIcon />} label="Comprovantes" />
+      <TableFilter />
+    </Box>
+  </Flex>
+);
