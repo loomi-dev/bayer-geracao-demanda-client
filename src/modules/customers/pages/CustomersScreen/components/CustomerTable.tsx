28c4948822d21169a3f62808ff05b8d902d72764
YanAnderson-bit <yan.anderson135@gmail.com>
2023-11-30 15:02:57 -0300

    Starts creating table section

---
+++ src/modules/customers/pages/CustomersScreen/components/CustomerTable.tsx
@@ -0,0 +1,7 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+export const CustomerTable = () => (
+  <Flex flexDir="column" w="100%" h="100%">
+    <Text textStyle="h4">Planejamentos</Text>
+  </Flex>
+);
