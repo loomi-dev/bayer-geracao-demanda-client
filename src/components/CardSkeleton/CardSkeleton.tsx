e1e34f5ad2da345b500a8b98cd771aca0ee1acf3
YanAnderson-bit <yan.anderson135@gmail.com>
2023-12-01 10:15:36 -0300

    Changes start skeleton colors

--- src/components/CardSkeleton/CardSkeleton.tsx
+++ src/components/CardSkeleton/CardSkeleton.tsx
@@ -2,4 +2,6 @@ import { Skeleton, SkeletonProps } from '@chakra-ui/react';
 
 export type CardSkeletonProps = SkeletonProps;
 
-export const CardSkeleton = ({ ...props }: CardSkeletonProps) => <Skeleton {...props} />;
+export const CardSkeleton = ({ ...props }: CardSkeletonProps) => (
+  <Skeleton startColor="surface.disabled" {...props} />
+);
