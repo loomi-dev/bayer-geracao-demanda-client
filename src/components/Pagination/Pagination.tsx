36f23f67e72e94840bfd14bf9e9cba078edee7e7
Bruno Guerra <bruno.dev8801@gmail.com>
2023-12-03 16:10:02 -0300

    feat: added count items and totalPages in Pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -5,14 +5,23 @@ import { ChevronLeftIcon, ChevronRightIcon } from '../icons';
 
 type PaginationProps = {
   page: number;
+  countItems: number;
+  totalPages: number;
   onNextPage: (e?: MouseEvent<HTMLButtonElement>) => void;
   onPreviousPage: (e?: MouseEvent<HTMLButtonElement>) => void;
 } & FlexProps;
 
-export const Pagination = ({ page, onPreviousPage, onNextPage, ...restProps }: PaginationProps) => (
+export const Pagination = ({
+  page,
+  countItems,
+  totalPages,
+  onPreviousPage,
+  onNextPage,
+  ...restProps
+}: PaginationProps) => (
   <Flex w="full" align="center" justify="flex-end" gap="2rem" px="2.4rem" {...restProps}>
     <Text textStyle="body3" color="text.footnote">
-      {page}-3 de 6
+      {page}-{countItems} de {totalPages}
     </Text>
 
     <IconButton variant="page" aria-label="Ir para página anterior" onClick={onPreviousPage}>
