30f378481fc90ce9e5a124d2721b2fcb60a4d4ac
YanAnderson-bit <yan.anderson135@gmail.com>
2023-12-04 20:57:31 -0300

    Puts pagination

--- src/modules/customers/pages/CustomerDetailScreen/components/CustomerPlanningTable/CustomerPlanningTable.tsx
+++ src/modules/customers/pages/CustomerDetailScreen/components/CustomerPlanningTable/CustomerPlanningTable.tsx
@@ -1,7 +1,8 @@
 import { Flex, Text } from '@chakra-ui/react';
 
 import { useGetFarmerPlans } from '@/api';
-import { DynamicTable } from '@/components';
+import { DynamicTable, Pagination } from '@/components';
+import { usePagination } from '@/hooks';
 
 import { customerPlanningColumns } from './CustomerPlanningTable.columns';
 
@@ -11,6 +12,7 @@ type CustomerPlanningTableProps = {
 
 export const CustomerPlanningTable = ({ customerId }: CustomerPlanningTableProps) => {
   const { data, isLoading } = useGetFarmerPlans({ farmerId: customerId });
+  const { currentPage, handleNextPage, handlePreviousPage } = usePagination();
   const plans = data?.data ?? [];
   return (
     <Flex flexDir="column" w="100%" gap="2.5rem" h="100%">
@@ -25,6 +27,13 @@ export const CustomerPlanningTable = ({ customerId }: CustomerPlanningTableProps
         fallbackMessage="Nenhum planejamento encontrado"
         fallbackProps={{ fontSize: { base: '1.2rem', '3xl': '1.6rem' } }}
         hoverProps={{ bgColor: 'opacity.green.0.10', cursor: 'pointer' }}
+      />
+      <Pagination
+        page={currentPage}
+        countItems={plans.length}
+        totalPages={data?.meta.pagination.pageCount ?? 1}
+        onNextPage={handleNextPage}
+        onPreviousPage={handlePreviousPage}
       />
     </Flex>
   );
