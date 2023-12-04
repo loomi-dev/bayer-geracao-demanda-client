6cf853b87f9c46c285817a44475aa29e6ddaed89
YanAnderson-bit <yan.anderson135@gmail.com>
2023-12-04 17:19:00 -0300

    Apply code review changes

--- src/modules/customers/pages/CustomerDetailScreen/CustomerDetailScreen.tsx
+++ src/modules/customers/pages/CustomerDetailScreen/CustomerDetailScreen.tsx
@@ -1,19 +1,22 @@
 import { useRouter } from 'next/router';
 
 import { useGetFarmer } from '@/api';
-import { BigChevronLeftIcon, Header } from '@/components';
+import { ChevronLeftIcon, Header } from '@/components';
 
 export const CustomerDetailScreen = () => {
   const { push, query } = useRouter();
-  const customerId = Number(query.id);
-  const { data, isLoading } = useGetFarmer({ farmerId: customerId });
+  const customerId = Number(query?.customer_id);
+  const { data, isLoading } = useGetFarmer(
+    { farmerId: customerId },
+    { enabled: Boolean(customerId) },
+  );
   const customer = data?.data[0];
   return (
     <>
       <Header
         label={`${customer?.name}`}
         onClick={() => push('/clientes')}
-        icon={<BigChevronLeftIcon />}
+        icon={<ChevronLeftIcon fontSize={36} color="white" />}
         isLoading={isLoading}
       />
     </>
