78acdf68d696151d03e76d083c1afd7d877e401d
YanAnderson-bit <yan.anderson135@gmail.com>
2023-12-06 10:12:05 -0300

    Applyu code review hcanges

--- src/modules/customers/pages/CustomerPlanningDetailScreen/CustomerPlanningDetailScreen.tsx
+++ src/modules/customers/pages/CustomerPlanningDetailScreen/CustomerPlanningDetailScreen.tsx
@@ -4,13 +4,13 @@ import { ChevronLeftIcon, Header } from '@/components';
 
 export const CustomerPlanningDetailScreen = () => {
   const { push, query } = useRouter();
-  const customer_id = Number(query.customer_id);
+  const customerId = Number(query.customer_id);
 
   return (
     <>
       <Header
         label="Detalhamento do planejamento"
-        onClick={() => push(`/clientes/${customer_id}`)}
+        onClick={() => push(`/clientes/${customerId}`)}
         icon={<ChevronLeftIcon fontSize={36} color="white" />}
       />
     </>
