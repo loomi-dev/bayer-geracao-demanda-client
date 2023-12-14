ad1e8804d1ce55efaa924bdea79e91eb1575a9d7
YanAnderson-bit <yan.anderson135@gmail.com>
2023-12-14 09:25:34 -0300

    Refactores Trousseau component to use hook

--- src/modules/trousseau/pages/TrousseauScreen/components/TrousseauOptionsSection/TrousseauOptionsSection.tsx
+++ src/modules/trousseau/pages/TrousseauScreen/components/TrousseauOptionsSection/TrousseauOptionsSection.tsx
@@ -1,41 +1,47 @@
 import { Divider, Flex, Text } from '@chakra-ui/react';
 
+import { useGetTrousseau } from '@/api';
 import { Trousseau } from '@/components';
-import { TROUSSEAU_LIST } from '@/config';
 
 import { TrousseauDownloadOption } from './TrousseauDownloadOption';
 import { TrousseauRecomendations } from './TrousseauRecomendations';
 
-export const TrousseauOptionsSection = () => (
-  <Flex
-    flexDir="column"
-    w="100%"
-    layerStyle="card"
-    gap="1.2rem"
-    bgColor="surface.primary"
-    p="2.4rem"
-  >
-    <Text textStyle="caption1" color="red.danger_90">
-      Tipos de enxoval
-    </Text>
-    <Divider w="full" borderColor="opacity.black.0.20" />
-    <Trousseau.Container>
-      <Trousseau.Slider trousseauList={TROUSSEAU_LIST}>
-        {({ label, image }) => (
-          <>
-            <Trousseau.Image src={image} />
-            <Trousseau.Label>{label}</Trousseau.Label>
-          </>
-        )}
-      </Trousseau.Slider>
-    </Trousseau.Container>
-    <Flex gap="0.8rem" mt="2rem" flexDir="column">
-      <TrousseauDownloadOption />
-      <TrousseauDownloadOption />
-      <TrousseauDownloadOption />
-      <TrousseauDownloadOption />
-    </Flex>
+export const TrousseauOptionsSection = () => {
+  const { data } = useGetTrousseau();
 
-    <TrousseauRecomendations />
-  </Flex>
-);
+  const material_items = data?.data.material_items;
+
+  return (
+    <Flex
+      flexDir="column"
+      w="100%"
+      layerStyle="card"
+      gap="1.2rem"
+      bgColor="surface.primary"
+      p="2.4rem"
+    >
+      <Text textStyle="caption1" color="red.danger_90">
+        Tipos de enxoval
+      </Text>
+      <Divider w="full" borderColor="opacity.black.0.20" />
+      <Trousseau.Container>
+        <Trousseau.Slider trousseauList={material_items}>
+          {({ name, url }) => (
+            <>
+              <Trousseau.Image src={url} />
+              <Trousseau.Label>{name}</Trousseau.Label>
+            </>
+          )}
+        </Trousseau.Slider>
+      </Trousseau.Container>
+      <Flex gap="0.8rem" mt="2rem" flexDir="column">
+        <TrousseauDownloadOption />
+        <TrousseauDownloadOption />
+        <TrousseauDownloadOption />
+        <TrousseauDownloadOption />
+      </Flex>
+
+      <TrousseauRecomendations />
+    </Flex>
+  );
+};
