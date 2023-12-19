794ffb75f628130ce6d6243e24363cecf5a920ee
YanAnderson-bit <yan.anderson135@gmail.com>
2023-12-19 19:58:17 -0300

    Fix header user menu fallback

--- src/components/UserProfileDrawer/components/ProfileImage.tsx
+++ src/components/UserProfileDrawer/components/ProfileImage.tsx
@@ -5,8 +5,9 @@ import { Avatar } from '@/components';
 
 type ProfileImageProps = {
   children?: ReactNode;
+  src?: string;
 } & StackProps;
-export const ProfileImage = ({ children, ...props }: ProfileImageProps) => (
+export const ProfileImage = ({ src = '', children, ...props }: ProfileImageProps) => (
   <VStack
     borderBottom="1px solid"
     borderBottomColor="greyscale.600"
@@ -16,7 +17,7 @@ export const ProfileImage = ({ children, ...props }: ProfileImageProps) => (
     pb="1.2rem"
     {...props}
   >
-    <Avatar imageFallbackSize={100} bg="white" />
+    <Avatar imageFallbackSize={80} boxSize="15rem" bg="white" />
     {children}
   </VStack>
 );
