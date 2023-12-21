617f2ffe0461027d5f44cc4118e893e17b73642d
YanAnderson-bit <yan.anderson135@gmail.com>
2023-12-21 15:50:33 -0300

    Removes unused mutation

--- src/api/auth/endpoints.ts
+++ src/api/auth/endpoints.ts
@@ -1,11 +1,6 @@
 import axios from '@/lib/axios';
 
-import {
-  LoginWithCredentialsData,
-  LoginWithCredentialsResponse,
-  UpdateUserData,
-  UpdateUserResponse,
-} from './types';
+import { LoginWithCredentialsData, LoginWithCredentialsResponse } from './types';
 
 export const loginWithCredentials = async (
   data: LoginWithCredentialsData,
@@ -15,21 +10,4 @@ export const loginWithCredentials = async (
     .post<LoginWithCredentialsResponse>('/auth/local', data);
 
   return response.data;
-};
-
-export const updateUser = async ({
-  id: userId,
-  username,
-  email,
-  phoneNumber,
-  company_position,
-}: UpdateUserData): Promise<UpdateUserResponse> => {
-  const { data } = await axios.authorized().put(`/users/${userId}`, {
-    username,
-    email,
-    phoneNumber,
-    company_position,
-  });
-
-  return data;
 };
--- src/api/auth/types.ts
+++ src/api/auth/types.ts
@@ -8,16 +8,3 @@ export type LoginWithCredentialsResponse = {
   jwt: string;
   user: User;
 };
-
-export type UpdateUserData = {
-  id: number;
-  username: string;
-  email: string;
-  company_position: string;
-  phoneNumber: string;
-};
-export type UpdateUserResponse = {
-  farmer: Farmer;
-  jwt: string;
-  user: User;
-};
