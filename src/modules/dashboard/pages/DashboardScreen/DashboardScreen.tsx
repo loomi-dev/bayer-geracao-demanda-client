150129f93f14c2933c195898c8a162a0f5e735b7
YanAnderson-bit <yan.anderson135@gmail.com>
2024-01-03 12:16:12 -0300

    Creates filters for dashboard

--- src/modules/dashboard/pages/DashboardScreen/DashboardScreen.tsx
+++ src/modules/dashboard/pages/DashboardScreen/DashboardScreen.tsx
@@ -1,8 +1,16 @@
+import { HStack } from '@chakra-ui/react';
+import { useState } from 'react';
+
 import { useGetDashboard } from '@/api';
+import { ActionFilter, CustomerFilter, RegionFilter } from '@/components';
 
 import { ActionsResumeSection, FinancialSection } from './components';
 
 export const DashboardScreen = () => {
+  const [customers, setCustomers] = useState<string[]>([]);
+  const [regions, setRegions] = useState<string[]>([]);
+  const [actions, setActions] = useState<string[]>([]);
+
   const { data, isLoading } = useGetDashboard();
   const actionsResumeSectionData = {
     plannedActionsQuantity: data?.data.plannedActionsQuantity ?? 0,
@@ -12,6 +20,11 @@ export const DashboardScreen = () => {
   };
   return (
     <>
+      <HStack w="100%" justify="flex-end">
+        <CustomerFilter selectedValues={customers} onSelect={setCustomers} />
+        <RegionFilter selectedValues={regions} onSelect={setRegions} />
+        <ActionFilter selectedValues={actions} onSelect={setActions} />
+      </HStack>
       <FinancialSection />
       <ActionsResumeSection data={actionsResumeSectionData} isLoading={isLoading} />
     </>
